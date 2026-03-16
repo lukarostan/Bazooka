1dafaf1ebc8bedcd9c63e3ddbf529a75633aaef4
Ubuntu <ubuntu@ip-172-31-6-169.us-east-2.compute.internal>
2026-03-16 15:02:10 +0000

    Configure Vite base path for GitHub Pages project URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bazooka/',
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
